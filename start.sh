012193c946642fb5b84ae8e0960b4adc229e906c
fabian <faberchri@gmail.com>
2012-12-06 15:07:04 +0100

    name of cluster as arg

--- start.sh
+++ start.sh
@@ -3,14 +3,46 @@
 type -P "starcluster" &>/dev/null || { echo "Err: Command not found: starcluster"
     echo "Install starcluster with command: easy_install starcluster"; exit 1; }
 
-if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied. Please pass the path to your starcluster config as first parameter"
-	exit 1
+outRootDir="/home/lsci/result" # default /<cycle-nr>/<job-id>
+workingDir="/root" # default
+inputDir="/home/lsci/worker" # default
+
+USAGE="Usage: `basename $0` <path/to/starcluster/config> nameOfCluster"
+
+# Parse command line options.
+while getopts hv OPT; do
+    case "$OPT" in
+        h)
+            echo $USAGE
+            exit 0
+            ;;
+        v)
+            echo "LSCI Project - Nicolas Bär - Fabian Christoffel, version 0.1"
+            exit 0
+            ;;
+        \?)
+            # getopts issues an error message
+            echo $USAGE >&2
+            exit 1
+            ;;
+    esac
+done
+
+# Remove the switches we parsed above.
+shift `expr $OPTIND - 1`
+
+# We want at least one non-option argument. 
+# Remove this block if you don't need it.
+if [ ! $# -eq 2 ]
+then
+    echo $USAGE >&2
+    exit 1
 fi
 
+# Access additional arguments as usual through 
+# variables $@, $*, $1, $2, etc. or using this loop:
 config=$1
-cluster="supercluster"
+cluster=$2
 
 # start cluster
 starcluster -c $config start $cluster
@@ -23,7 +55,7 @@ starcluster -c $config sshmaster $cluster 'rm -rf /home/lsci && mkdir /home/lsci
 # master files
 starcluster -c $config put $cluster --node master master /home/lsci/master
 # worker files
-starcluster -c $config put $cluster worker /home/worker
+starcluster -c $config put $cluster worker /home/lsci/worker
 
 
 
